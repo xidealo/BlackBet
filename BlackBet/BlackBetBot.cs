f1b96c15c67c8acc37160c80b08eaa056acca42f
idealik <shavl.mark@yandex.ru>
2019-03-24 17:21:22 +0300

    first commit

---
+++ BlackBet/BlackBetBot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BlackBet
+{
+    class BlackBetBot
+    {
+        IWebDriver browser;
+
+        public void start()
+        {
+            //открываем браузер
+            openBrowser();
+
+            Thread.Sleep(1000);
+        }
+
+        private void openBrowser()
+        {
+            // открыть вк 
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+
+            //открываю браузер на весь экран
+            browser.Manage().Window.Maximize();
+
+            // отправляемся по ссылке
+            browser.Navigate().GoToUrl("https://www.web-telegram.ru");
+
+        }
+    }
+}
---
+++ BlackBet/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackBet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        BlackBetBot blackBetBot;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blackBetBot = new BlackBetBot();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            blackBetBot.start();
+        }
+    }
+}
