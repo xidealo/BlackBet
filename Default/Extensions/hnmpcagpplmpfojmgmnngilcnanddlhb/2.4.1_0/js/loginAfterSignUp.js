384ada91aff590adf5c1f98408ba3f47f3477cd7
Valentin Podkopaev <max.astin@yandex.ru>
2019-03-26 23:48:10 +0300

    удалена папка с браузером

--- Default/Extensions/hnmpcagpplmpfojmgmnngilcnanddlhb/2.4.1_0/js/loginAfterSignUp.js
+++
@@ -1,4 +0,0 @@
-var cookie = document.getElementById("ws_ext_auth") && document.getElementById("ws_ext_auth").value;
-
-cookie && chrome.runtime.sendMessage({action: "LOGIN_AFTER_SIGNUP", cookie: cookie});
-console.log('content_script_after_signup');
